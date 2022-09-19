8d1355a11d366c1000efa57e02b3888b2e23c497
CarlosMenke <carlos.menke@t-online.de>
2022-09-19 08:48:46 +0200

    Login Function: Make debug print more verbose and pritty

--- backend/src/handler/api.rs
+++ backend/src/handler/api.rs
@@ -6,7 +6,10 @@ use crate::{auth::*, errors::ServiceError};
 pub async fn login(
     user_permissions: web::Json<UserPermissions>,
 ) -> Result<web::Json<UserPermissionsResponse>, ServiceError> {
-    debug!("login function called.");
+    debug!(
+        "login function called for User: {:#?} with Permisstions: {:#?}",
+        &user_permissions.username, &user_permissions.permissions
+    );
     let token_str = create_token(
         user_permissions.username.clone(),
         user_permissions.permissions.clone(),
