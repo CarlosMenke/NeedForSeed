52c1c3e753b0070aa7ee4080826a04990772b16a
CarlosMenke <carlos.menke@t-online.de>
2022-09-27 22:32:37 +0200

    cleanup for unused middleware

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,9 +3,7 @@ extern crate dotenvy;
 extern crate serde;
 
 use actix_cors::Cors;
-use actix_identity::IdentityMiddleware;
-use actix_session::{storage::RedisActorSessionStore, SessionMiddleware};
-use actix_web::{cookie::Key, web, App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use actix_web_httpauth::middleware::HttpAuthentication;
 
 use diesel::{r2d2, r2d2::ConnectionManager, PgConnection};
@@ -31,13 +29,13 @@ async fn main() -> std::io::Result<()> {
     env_logger::builder().format_timestamp(None).init();
 
     let settings = Application::default();
-    let private_key = Key::generate();
 
     let connection_manager = ConnectionManager::<PgConnection>::new(settings.database_url);
     let pool: Pool = r2d2::Pool::builder()
         .build(connection_manager)
         .expect("Failed to create pool.");
 
+    //add https support
     HttpServer::new(move || {
         let cors = Cors::permissive()
             //.allow_any_origin()
@@ -52,14 +50,6 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .wrap(cors)
-            .wrap(IdentityMiddleware::default())
-            .wrap(
-                SessionMiddleware::builder(
-                    RedisActorSessionStore::new(settings.redis_uri.clone()),
-                    private_key.clone(),
-                )
-                .build(),
-            )
             .service(
                 web::scope("/api")
                     .route("login.json", web::post().to(api::login))
@@ -70,6 +60,7 @@ async fn main() -> std::io::Result<()> {
                     )),
             )
     })
+    .workers(2)
     .bind(format!("{}:{}", settings.server_ip, settings.server_port))
     .expect("Can not bind to IP:PORT")
     .run()
