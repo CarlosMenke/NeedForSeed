450620c544419325540001861bf42c43835ee949
CarlosMenke <carlos.menke@t-online.de>
2023-01-05 12:44:13 +0100

    Make page not found readable on dark background.

--- frontend/src/page/not_found.rs
+++ frontend/src/page/not_found.rs
@@ -1,14 +1,15 @@
 use seed::{prelude::*, *};
 
 pub fn view<Ms>() -> Node<Ms> {
-    section![C!["hero", "is-medium", "ml-6"],
-        div![C!["hero-body"],
-            h1![C!["title", "is-size-1"],
-                "404",
-            ],
-            h2![C!["subtitle", "is-size-3"],
-                "Page not found",
-            ]
+    section![
+        C!["hero", "is-medium", "ml-6"],
+        div![
+            C!["hero-body"],
+            style! {
+                St::Color => "white",
+            },
+            h1![C!["title", "is-size-1"], "404",],
+            h2![C!["subtitle", "is-size-3"], "Page not found",]
         ]
     ]
 }
