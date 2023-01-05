e1b02958b6e024a6b35f1627c76a202b4948644c
CarlosMenke <carlos.menke@t-online.de>
2023-01-05 15:43:42 +0100

    Correct name of redis url.

--- backend/src/configuration.rs
+++ backend/src/configuration.rs
@@ -2,7 +2,7 @@ use std::env;
 
 #[derive(Debug)]
 pub struct Application {
-    pub redis_uri: String,
+    pub redis_url: String,
     pub domain: String,
     pub database_url: String,
     pub server_ip: String,
@@ -12,7 +12,7 @@ pub struct Application {
 impl Default for Application {
     fn default() -> Application {
         Application {
-            redis_uri: env::var("REDIS_URL").expect("REDIS_URL must be set in .env."),
+            redis_url: env::var("REDIS_URL").expect("REDIS_URL must be set in .env."),
             domain: env::var("DOMAIN").expect("DMOAIN must be set in .env."),
             database_url: env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env"),
             server_ip: env::var("SERVER_IP").expect("SERVER_IP must be set in .evn."),
